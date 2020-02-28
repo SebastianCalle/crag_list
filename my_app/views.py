bb0eae0365c771845e4fc3ecaafe49b16dbdc866
SebastianCalle <jusemonca@gmail.com>
2020-02-28 15:04:53 -0500

    [Feature]: Create app and the models for search with his view

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    # View for home
+    return render(request, 'base.html' )
+
+
+def new_search(request):
+    # View for new_search
+    pass
