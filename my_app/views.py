79079b87e44f585a5d03f2a6c4b4c5137e77ad0f
SebastianCalle <jusemonca@gmail.com>
2020-02-29 17:47:25 -0500

    [Feature]: Create the new_Search view and his respective temṕlate

--- my_app/views.py
+++ my_app/views.py
@@ -5,7 +5,10 @@ def home(request):
     # View for home
     return render(request, 'base.html' )
 
-
 def new_search(request):
     # View for new_search
-    pass
+    search = request.POST.get('search')
+    context = {
+        'search':search,
+    }
+    return render(request, 'my_app/new_search.html', context)
