7c81940e75fe9d41b2e712dae2eb62643738dcd2
SebastianCalle <jusemonca@gmail.com>
2020-03-01 12:03:05 -0500

    [Feature]: Scraping img of each post from the cragslist and show in the front-end

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-import  requests
+import requests
+from requests.compat import quote_plus
 from bs4 import BeautifulSoup
+from . import models
+import re
 
-# Create your views here.
+url = 'https://losangeles.craigslist.org/search/sss?query={}'
+
 def home(request):
     # View for home
     return render(request, 'base.html' )
@@ -10,7 +14,28 @@ def home(request):
 def new_search(request):
     # View for new_search
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = url.format(quote_plus(search))
+    res = requests.get(final_url)
+    data = res.text
+    soup = BeautifulSoup(data, features='html.parser')
+    search_list = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+    for post in search_list:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        post_img = post.find(class_='result-image').get('data-ids')
+        post_price = post.find(class_='result-price').text
+        if not post_img :
+            post_img = "peace,None"
+        img_id = re.split(',|:',post_img)
+        src_img = "https://images.craigslist.org/{}_300x300.jpg".format(img_id[1])
+        final_postings.append((post_title, post_url, post_price, src_img))
+
+
     context = {
         'search':search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', context)
